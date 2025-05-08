da1ad929257548e36f8c6840865d221d386cd9fd
Abiatar Cosma <abiatar.cosma@yahoo.com>
2025-05-08 18:35:37 +0200

    Add JWT authentication middleware and logout/current user routes
    - Implemented authenticate middleware to protect secure routes
    - Created /users/current to get info about the logged-in user
    - Created /users/logout to clear the user's token and end session
    - Tested all JWT flows using Postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const signup = require("../../controllers/users/signup");
 const login = require("../../controllers/users/login");
+const logout = require("../../controllers/users/logout");
+const authenticate = require("../../middlewares/authenticate");
+const getCurrent = require("../../controllers/users/getCurrent");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/logout", authenticate, logout);
+router.get("/current", authenticate, getCurrent);
 
 module.exports = router;
